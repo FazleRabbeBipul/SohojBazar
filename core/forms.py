65ac53ff9a3d72dede2fee1f7d2f8dbc3069ca10
FazleRabbeBipul <rabbecse1677@gmail.com>
2024-01-25 05:10:05 +0600

    Signup form + new users added

--- core/forms.py
+++ core/forms.py
@@ -6,3 +6,23 @@ class SignupForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')   
+
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Your username',
+        'class': 'w-full py-4 px-6 rounded-xl'
+    })) 
+
+    email = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Your email address',
+        'class': 'w-full py-4 px-6 rounded-xl'
+    })) 
+
+    password1 = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Your password',
+        'class': 'w-full py-4 px-6 rounded-xl'
+    })) 
+
+    password2 = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Repeat password',
+        'class': 'w-full py-4 px-6 rounded-xl'
+    })) 
--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 # core/views.py
@@ -19,7 +19,14 @@ def contact(request):
     return render(request, 'core/contact.html')
 
 def signup(request):
-    form = SignupForm()
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('/login/')
+    else:
+        form = SignupForm() 
 
     return render(request, 'core/signup.html', {
         'form' : form
