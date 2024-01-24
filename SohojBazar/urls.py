e3ed39b93dc366aa051a739162e48806d6b398bf
FazleRabbeBipul <rabbecse1677@gmail.com>
2024-01-24 21:22:06 +0600

    Products + admin  +  viewing working

--- SohojBazar/urls.py
+++ SohojBazar/urls.py
@@ -1,4 +1,8 @@
 # project/urls.py
+
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.contrib import admin
 from django.urls import path
 
@@ -8,4 +12,4 @@ urlpatterns = [
     path('', index, name='index'),
     path('contact/', contact, name='contact'),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- core/views.py
+++ core/views.py
@@ -3,9 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 # core/views.py
 
+from item.models import Category, Item
 
 def index(request):
-    return render(request, 'core/index.html')
+    items = Item.objects.filter(is_sold = False)[0:9]
+    categories = Category.objects.all()
+
+    return render(request, 'core/index.html',{
+        'categories' : categories,
+        'items' : items,
+    })
 
 def contact(request):
     return render(request, 'core/contact.html')
